c8309cd678fce5219706f30ea5e1a708269f8632
AnTxApple <110225353+AnTxApple@users.noreply.github.com>
2022-07-29 17:05:24 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -36,8 +36,8 @@ $(document).ready(function () {
       title: textConfig.text1,
       text: textConfig.text2,
       imageUrl: "file:///C:/Users/PC/Downloads/background.webp",
-      imageWidth: 300,
-      imageHeight: 300,
+      imageWidth: 100,
+      imageHeight: 100,
       background: '#fff url("img/iput-bg.jpg")',
       imageAlt: "Custom image",
     }).then(function () {
