79a8051e3618f64785b2db7b0dd76b53186e1b95
AnTxApple <110225353+AnTxApple@users.noreply.github.com>
2022-07-29 19:48:46 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
       title: textConfig.text1,
       text: textConfig.text2,
       imageUrl: "img/cuteCat.jpg",
-      imageWidth: 50,
+      imageWidth: 200,
       imageHeight: 500,
       background: '#fff url("img/iput-bg.jpg")',
       imageAlt: "Custom image",
